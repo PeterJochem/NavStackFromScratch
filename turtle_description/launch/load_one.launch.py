1360e2107130281d4081996685cd422fe42df704
Peter Jochem <peterpennrichjochem@gmail.com>
2023-11-04 20:12:42 -0400

    Adds the use joint state publisher arg.

--- turtle_description/launch/load_one.launch.py
+++ turtle_description/launch/load_one.launch.py
@@ -9,7 +9,8 @@ import os
 def generate_launch_description():
 
     
-    use_sim_time = DeclareLaunchArgument('use_sim_time', default_value='true', description='Use simulation (Gazebo) clock if true')
+    use_sim_time = DeclareLaunchArgument('use_sim_time', default_value='true', description='Controls if the simulation (Gazebo) clock is used.')
+    use_jsp = DeclareLaunchArgument('use_jsp', default_value='true', description='Controls if the joint state publisher is launched to publish default joint values.')
     rviz_arg = DeclareLaunchArgument('start_rviz', default_value='true', description='Controls whether to start rviz2 or not.')
     rviz_config_dir = DeclareLaunchArgument('rviz_config_dir', default_value='true', description='Location of RVIZ config.')
     use_jsp = DeclareLaunchArgument('use_jsp', default_value='true', description='Controls whether the joint state publisher is used to publish default joint states.')
@@ -27,16 +28,19 @@ def generate_launch_description():
         robot_description = infp.read()
 
     rsp_params = {'robot_description': robot_description}
-    #breakpoint()
     robot_state_publisher = Node(package='robot_state_publisher', executable='robot_state_publisher', output='screen', parameters=[rsp_params, {'use_sim_time': launch.substitutions.LaunchConfiguration('use_sim_time')}]) 
 
     rviz_node = Node(package='rviz2', executable='rviz2', name='rviz2', output='screen', arguments=['-d', [os.path.join(get_package_share_directory('turtle_description'), 'config', 'config.rviz')]], on_exit=launch.actions.Shutdown())
+    joint_state_publisher = Node(package="joint_state_publisher",
+                                executable="joint_state_publisher",
+                                name="joint_state_publisher")
 
 
     return LaunchDescription([DeclareLaunchArgument('use_sim_time', default_value="false", description='Use simulation (Gazebo) clock if true'),
                                 rviz_arg,
                                 use_jsp,
                                 rviz_node,
+                                joint_state_publisher,
                                 robot_state_publisher
                                 ])
 
