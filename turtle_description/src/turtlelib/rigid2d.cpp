4adf9207f9fa0b4169f9b948f29485996b5c0b2b
Peter Jochem <peterpennrichjochem@gmail.com>
2023-11-05 17:22:07 -0500

    Fixes bug computing the angle of rotation from a 2D transformation matrix.

--- turtle_description/src/turtlelib/rigid2d.cpp
+++ turtle_description/src/turtlelib/rigid2d.cpp
@@ -3,6 +3,17 @@
 namespace turtlelib
 {
 
+double rotation_angle(const Eigen::MatrixXd& matrix) {
+
+    if (matrix.cols() != 3 || matrix.rows() != 3) {
+       throw std::runtime_error("The Eigen matrix must represent a 2D transformation."); 
+    }
+
+    double cos_theta = matrix(0, 0);
+    double sin_theta = matrix(1, 0);
+    return std::atan2(sin_theta, cos_theta);
+}
+
 std::ostream & operator<<(std::ostream & os, const Vector2D & v) {
     os << "[" << v.x << ", " << v.y << "]";
     return os;
@@ -48,6 +59,198 @@ std::istream & operator>>(std::istream & is, Vector2D & v) {
     return is;
 }
 
+Vector2D::Vector2D(double x, double y) {
+    this->x = x;
+    this->y = y;
+}
+
+Vector2D::Vector2D() {
+    this->x = 0.0;
+    this->y = 0.0;
+}
+
+Transform2D::Transform2D() {
+    matrix = Eigen::MatrixXd::Identity(3, 3);
+
+}
+
+void Transform2D::set_rotation(Eigen::MatrixXd& matrix, double radians) {
+    double cos_theta = std::cos(radians);
+    double sin_theta = std::sin(radians);
+    matrix(0, 0) = cos_theta;
+    matrix(0, 1) = -sin_theta;
+    matrix(1, 0) = sin_theta;
+    matrix(1, 1) = cos_theta; 
+}
+
+void Transform2D::set_translation(Eigen::MatrixXd& matrix, Vector2D trans) {
+    matrix(0, 2) = trans.x;
+    matrix(1, 2) = trans.y;
+}
+
+void Transform2D::set_matrix(Eigen::MatrixXd matrix) {
+    this->matrix = matrix;
+}
+
+Transform2D::Transform2D(Vector2D trans) {
+    matrix = Eigen::MatrixXd::Identity(3, 3);
+    set_translation(matrix, trans);
+}
+
+Transform2D::Transform2D(double radians) {
+    matrix = Eigen::MatrixXd::Identity(3, 3);
+    set_rotation(matrix, radians);
+}
+
+
+Transform2D::Transform2D(Vector2D trans, double radians) {
+    matrix = Eigen::MatrixXd::Identity(3, 3);
+    double cos_theta = std::cos(radians);
+    double sin_theta = std::cos(radians);
+    matrix(0, 0) = cos_theta;
+    matrix(0, 1) = -sin_theta;
+    matrix(1, 0) = sin_theta;
+    matrix(1, 1) = cos_theta; 
+}
+
+Vector2D Transform2D::operator()(Vector2D v) const {
+
+    auto homogenous_column_vector = Eigen::MatrixXd(3, 1);
+    homogenous_column_vector(0, 0) = v.x;
+    homogenous_column_vector(0, 1) = v.y;
+    homogenous_column_vector(0, 2) = 1.0;
+
+    auto v_transformed = matrix * homogenous_column_vector;
+    double x = v_transformed(0, 0);
+    double y = homogenous_column_vector(0, 1);
+    return Vector2D(x, y);
+}
+
+Transform2D Transform2D::inv() const {
+    
+    double theta = rotation_angle(matrix);
+    /*
+    auto transform = translation();
+    double cos_theta = cos(theta);
+    double sin_theta = sin(theta);
+    auto inverse_matrix = Eigen::MatrixXd::Identity(3, 3);
+
+    inverse_matrix(0, 0) = cos_theta; 
+    inverse_matrix(1, 0) = -sin_theta;
+    
+    inverse_matrix(0, 1) = sin_theta;
+    inverse_matrix(1, 1) = cos_theta;
+
+    inverse_matrix(0, 2) = (-transform.x * cos_theta) - (transform.y * sin_theta);
+    inverse_matrix(1, 2) = (-transform.y * cos_theta) + (transform.x * sin_theta);
+    */
+
+    auto inverse_matrix = matrix.inverse();
+    Transform2D inverse_transform = Transform2D();
+    inverse_transform.set_matrix(inverse_matrix);
+    return inverse_transform;
+}
+
+Eigen::MatrixXd Transform2D::get_matrix() const {
+    return matrix;
+}
+
+Transform2D Transform2D::operator*(const Transform2D & rhs) const {
+    auto resulting_matrix = matrix * rhs.get_matrix();
+    Transform2D result;
+    result.set_matrix(resulting_matrix);
+    return result;
+}
+
+Transform2D & Transform2D::operator*=(const Transform2D & rhs) {
+    auto resulting_matrix = matrix * rhs.get_matrix();
+    set_matrix(resulting_matrix);
+    return *this;
+}
+
+void Transform2D::set_translation(Vector2D vector) {
+    set_translation(matrix, vector);
+}
+
+void Transform2D::set_rotation(double radians) {
+    set_rotation(matrix, radians);
+}
+
+
+
+Vector2D Transform2D::translation() const {
+    double x = matrix(0, 2);
+    double y = matrix(1, 2);
+    return Vector2D(x, y);
+}
+
+double Transform2D::rotation() const {
+    return rotation_angle(matrix);
+}
+
+std::ostream & operator<<(std::ostream & os, const Transform2D & transform) {
+
+    auto translation = transform.translation();
+    double rotation = transform.rotation();
+    os << "Translation: " << "[" << translation.x << ", " << translation.y << "]" << std::endl;
+    os << "Rotation (degrees): " << rad2deg(rotation) << std::endl;
+    return os;
+}
+
+Transform2D operator*(Transform2D& lhs, const Transform2D & rhs) {
+    return lhs * rhs;
+}
+
+std::istream & operator>>(std::istream & is, Transform2D & transform) {
+
+    std::string s;
+    std::getline(is, s);
+
+    if (s.size() < 9) {
+        throw std::runtime_error("The input stream is too short to contain a Transform2D.");
+    }
+
+    bool has_opening_bracket = s[0] == '[';
+    bool has_closing_bracket = s[s.size() - 1] == ']';
+
+    if (has_opening_bracket != has_closing_bracket) {
+        throw std::runtime_error("The stream does not conform to the input pattern for a Transform2D.");
+    }
+    else if (has_opening_bracket && has_closing_bracket) {
+        s = s.substr(1, s.size() - 2);
+    }
+
+    std::string delimiter = ",";
+    size_t pos = 0;
+    std::string x, y, angle, rest;
+    if ((pos = s.find(delimiter)) != std::string::npos) {
+        x = s.substr(0, pos);
+        rest = s.substr(pos + 1, s.size());
+    }
+    else { 
+        throw std::runtime_error("The stream does not conform to the input pattern for a Transform2D.");
+    }
+
+    if ((pos = rest.find(delimiter)) != std::string::npos) {
+        y = rest.substr(0, pos);
+        angle = rest.substr(pos + 1, rest.size());
+    }
+    else { 
+        throw std::runtime_error("The stream does not conform to the input pattern for a Transform2D.");
+    }
+
+    // Remove whitespace
+    x.erase(std::remove(x.begin(), x.end(), ' '), x.end());
+    y.erase(std::remove(y.begin(), y.end(), ' '), y.end());
+    angle.erase(std::remove(angle.begin(), angle.end(), ' '), angle.end());
+
+    std::cout << x << y << angle << std::endl << std::flush;
+
+    transform.set_translation(Vector2D(stod(x), stod(y)));
+    transform.set_rotation(stod(angle));
+
+    return is;
+}
 
 }
 
@@ -60,9 +263,13 @@ int main() {
     v.y = 2;
     std::cout << v << std::endl;
 
-    std::cin >> v;
-    std::cout << v << std::endl;
+    //std::cin >> v;
+    //std::cout << v << std::endl;
 
+    Transform2D transform = Transform2D();
+    std::cin >> transform;
+    std::cout << transform << std::endl;
+    std::cout << transform.matrix << std::endl;
 
     return 0;
 }
--- turtle_description/src/turtlelib/rigid2d.hpp
+++ turtle_description/src/turtlelib/rigid2d.hpp
@@ -13,6 +13,8 @@
 #include <cstring>
 #include <algorithm>
 #include <string>
+#include <Eigen/Dense>
+#include <cmath>
 
 namespace turtlelib
 {
@@ -59,9 +61,14 @@ namespace turtlelib
 
         /// \brief the y coordinate
         double y = 0.0;
+        Vector2D(double, double);
+        Vector2D();
     };
 
-
+    /// \brief Computes the rotation about the z-axis from a 2D rotation matrix.
+    /// \param Eigen::MatrixXd - Represents a 2D rotation matrix. 
+    /// \return - The angle in radians.
+    double rotation_angle(Eigen::MatrixXd&);
 
     /// \brief output a 2 dimensional vector as [xcomponent ycomponent]
     /// os - stream to output to
@@ -73,20 +80,6 @@ namespace turtlelib
     ///   [x y] or x y
     /// \param is - stream from which to read
     /// \param v [out] - output vector
-    ///
-    /// The way input works is (more or less): what the user types is stored in a buffer until the user types
-    /// a newline (by pressing enter).  The iostream methods then process the data in this buffer character by character.
-    /// Typically, each character is examined and then removed from the buffer automatically.
-    /// If the characters don't match what is expected (e.g., we are expecting an int but the letter 'q' is encountered)
-    /// an error flag is set on the stream object (e.g., std::cin) and processing stops.
-    ///
-    /// We have lower level control however.
-    /// std::peek() looks at the next unprocessed character in the buffer without removing it
-    ///     https://en.cppreference.com/w/cpp/io/basic_istream/peek
-    /// std::get() removes the next unprocessed character from the buffer.
-    ///     https://en.cppreference.com/w/cpp/io/basic_istream/get
-    /// When you call std::peek() it will wait for there to be at least one character in the buffer (e.g., the user types a character)
-    /// HINT: this function can be written in under 20 lines and uses only std::peek(), std::get(), istream::operator>>() and a little logic
     std::istream & operator>>(std::istream & is, Vector2D & v);
 
     /// \brief a rigid body transformation in 2 dimensions
@@ -126,6 +119,10 @@ namespace turtlelib
         /// \return a reference to the newly transformed operator
         Transform2D & operator*=(const Transform2D & rhs);
 
+        Transform2D operator*(const Transform2D & rhs) const;
+
+        Eigen::MatrixXd get_matrix() const;
+
         /// \brief the translational component of the transform
         /// \return the x,y translation
         Vector2D translation() const;
@@ -138,6 +135,16 @@ namespace turtlelib
         /// for a description
         friend std::ostream & operator<<(std::ostream & os, const Transform2D & tf);
 
+        void set_translation(Vector2D);
+        void set_rotation(double);
+
+        Eigen::MatrixXd matrix;
+
+        private:
+            //Eigen::MatrixXd matrix;
+            void set_rotation(Eigen::MatrixXd&, double radians);
+            void set_translation(Eigen::MatrixXd&, Vector2D trans);
+            void set_matrix(Eigen::MatrixXd);
     };
 
 
@@ -159,11 +166,7 @@ namespace turtlelib
     /// \param lhs - the left hand operand
     /// \param rhs - the right hand operand
     /// \return the composition of the two transforms
-    /// HINT: This function should be implemented in terms of *=
-    Transform2D operator*(Transform2D lhs, const Transform2D & rhs);
-
-
-
+    Transform2D operator*(Transform2D& lhs, const Transform2D & rhs);
 }
 
 #endif
