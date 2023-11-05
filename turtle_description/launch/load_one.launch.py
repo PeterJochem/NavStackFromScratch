c6f47e745d12e9e3f753e792d1a5ef607317d422
Peter Jochem <peterpennrichjochem@gmail.com>
2023-11-04 20:01:20 -0400

    Sketches the turtle description package.

---
+++ turtle_description/launch/load_one.launch.py
@@ -0,0 +1,43 @@
+import launch
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+import os
+
+def generate_launch_description():
+
+    
+    use_sim_time = DeclareLaunchArgument('use_sim_time', default_value='true', description='Use simulation (Gazebo) clock if true')
+    rviz_arg = DeclareLaunchArgument('start_rviz', default_value='true', description='Controls whether to start rviz2 or not.')
+    rviz_config_dir = DeclareLaunchArgument('rviz_config_dir', default_value='true', description='Location of RVIZ config.')
+    use_jsp = DeclareLaunchArgument('use_jsp', default_value='true', description='Controls whether the joint state publisher is used to publish default joint states.')
+        
+
+    # Configure the robot state publisher.
+    urdf_file_name = "turtlebot3_burger.urdf"
+    print(f"urdf_file_name : urdf_file_name")
+    urdf = os.path.join(
+        get_package_share_directory('turtle_description'),
+        'urdf',
+        urdf_file_name)
+
+    with open(urdf, 'r') as infp:
+        robot_description = infp.read()
+
+    rsp_params = {'robot_description': robot_description}
+    #breakpoint()
+    robot_state_publisher = Node(package='robot_state_publisher', executable='robot_state_publisher', output='screen', parameters=[rsp_params, {'use_sim_time': launch.substitutions.LaunchConfiguration('use_sim_time')}]) 
+
+    rviz_node = Node(package='rviz2', executable='rviz2', name='rviz2', output='screen', arguments=['-d', [os.path.join(get_package_share_directory('turtle_description'), 'config', 'config.rviz')]], on_exit=launch.actions.Shutdown())
+
+
+    return LaunchDescription([DeclareLaunchArgument('use_sim_time', default_value="false", description='Use simulation (Gazebo) clock if true'),
+                                rviz_arg,
+                                use_jsp,
+                                rviz_node,
+                                robot_state_publisher
+                                ])
+
+generate_launch_description()
